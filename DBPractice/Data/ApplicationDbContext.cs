cd4597f022437a0223e20fd64d065a120e0af16f
Mezerg <j.kwiatkowski122@gmail.com>
2024-02-14 20:18:11 +0100

    Dodaj pliki projektów.

---
+++ DBPractice/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DBPractice.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBPractice.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
